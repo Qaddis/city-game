ec21bdc5d4d1e0ff13f447e209ad8d77eab16c03
Qaddis <svyatoslavb1107@gmail.com>
2024-04-29 23:37:19 +0300

    Added getting countries and capitals and writing them into the dictionary (main.py); added interaction with the interface and game logic (script.js)

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import eel
+import random
 import wiki_parser
 
+capitals = wiki_parser.get_capitals()
+
+
+@eel.expose
+def get_random_capital():
+    random_country = random.choice(list(capitals.keys()))
+    random_capital = capitals[random_country]
+
+    return [random_country, random_capital]
+
+
 if __name__ == "__main__":
-	print(wiki_parser.get_capitals())
+    eel.init('web')
+    eel.start('index.html', size=(800, 500))
---
+++ web/script.js
@@ -0,0 +1,42 @@
+const scoreOutput = document.getElementById("score");
+const countryOutput = document.getElementById("contry");
+const capitalInput = document.getElementById("capital");
+
+let answer = "";
+let score = 0;
+
+const changeTheme = () => {
+	document.body.classList.add("false");
+	setTimeout(() => document.body.classList.remove("false"), 1500);
+};
+
+const checkAnswer = () => {
+	let userAnswer = capitalInput.value.trim();
+
+	if (userAnswer.length > 3) {
+		if (userAnswer.toLowerCase() === answer.toLowerCase()) {
+			capitalInput.value = "";
+			score++;
+			scoreOutput.innerHTML = `Счёт: ${score}`;
+
+			getRandomCapital();
+		} else {
+			capitalInput.value = "";
+			score--;
+			scoreOutput.innerHTML = `Счёт: ${score}`;
+
+			changeTheme();
+		}
+	}
+};
+
+function getRandomCapital() {
+	eel
+		.get_random_capital()()
+		.then((value) => {
+			countryOutput.innerHTML = value[0];
+			answer = value[1];
+		});
+}
+
+getRandomCapital();
